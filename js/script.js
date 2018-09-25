32cf1abec0a232c2975b46b16d402b7eda7f3eeb
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-25 15:23:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,35 @@
 $(document).ready(function(){
 	console.log('DOM loaded');
-});
-
-var url = 'https://restcountries.eu/rest/v1/name/';
-var countriesList = $('#countries');
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var countriesList = $('#countries');
 
 
-$('#search').click(searchCountries);
+	$('#search').click(searchCountries);
 
 
 
-function searchCountries() {
- 	var countryName = $('#country-name').val();
-  if(!countryName.length) countryName = 'Poland';
-}
+	function searchCountries() {
+ 		var countryName = $('#country-name').val();
+  	if(!countryName.length) countryName = 'Poland';
+	}
 
-function searchCountries() {
- 	var countryName = $('#country-name').val();
-if(!countryName.length) countryName = 'Poland';
-$.ajax({
+	function searchCountries() {
+ 		var countryName = $('#country-name').val();
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
   		url: url + countryName,
   		method: 'GET',
   		success: showCountriesList
   	});
-}
+	}
 
 
-function showCountriesList(resp) {
-    countriesList.empty();
-    resp.forEach(function(item) {
-     $('<li>').text(item.name).appendTo(countriesList);
+	function showCountriesList(resp) {
+    		countriesList.empty();
+    		resp.forEach(function(item) {
+     	$('<li>').text(item.name).appendTo(countriesList);
     });
 };
+});
+
+
